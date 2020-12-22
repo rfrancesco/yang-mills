fdf37a64f6f3a13932a03d432d6f1578a00bc487
Rocco Francesco Basta <r.basta@studenti.unipi.it>
2020-12-22 16:07:23 +0100

    Fixed critical typo

--- lib/gauge_conf_meas.c
+++ lib/gauge_conf_meas.c
@@ -1022,7 +1022,6 @@ void perform_measures_localobs_with_tracedef(Gauge_Conf const * const GC,
       {
       fprintf(datafilep, "%.12g %.12g ", polyre_2[i], polyim_2[i]);
       }
-   }
 
    // topological observables
    #if( (STDIM==4 && NCOLOR>1) || (STDIM==2 && NCOLOR==1) )
